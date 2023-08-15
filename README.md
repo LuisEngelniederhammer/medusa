9f6cf77ab2a45f14dc75b84bf464372ce3c1a30a
Luis Engelniederhammer <luis.engelniederhammer@posteo.de>
2023-08-15 17:47:46 +0000

    testing rocket.rs as replacement for actix as webframework

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Medusa
 WIP
+
+
+## WebAPI description
+### Authentication
+
+### Specs
+___
+#### POST /
+##### Request
+>TBD: upload a valid imagefile as plaintext encoded in base64 or within a json structure?
+>TBD: HTTP Method POST or PUT
+##### Repsonse
+>returns a uuid named `upload-id`
+___
+#### GET /status/{upload-id}
+##### Request
+##### Repsonse
+___
+#### GET /
+
+### Data structs
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,22 @@
-use std::{collections::HashMap, time::Duration};
+#[macro_use] extern crate rocket;
+use rocket::{Data, data::ToByteUnit};
 
-use actix::{Actor, AsyncContext, StreamHandler};
-use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
-use actix_web_actors::ws::{self, WebsocketContext};
 
-enum Function {
-    Authentication,
-}
-struct Package {
-    function: Function,
-}
+#[post("/", format = "plain", data = "<data>")]
+async fn upload(data: Data<'_>) -> String{
+    let result_request_data = data
+    .open(5.megabytes())
+    .into_string().await;  
 
-struct PeerMap<'a> {
-    pub peers: HashMap<String, &'a mut WebsocketContext<HermesBackendService>>,
-}
-
-struct HermesBackendService;
-
-impl HermesBackendService {}
-
-impl Actor for HermesBackendService {
-    type Context = ws::WebsocketContext<Self>;
-}
-
-/// Handler for ws::Message message
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for HermesBackendService {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        match msg {
-            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(_text)) => {
-                ctx.run_interval(Duration::from_secs(5), |_act, ctx| {
-                    //Do this in started within the Actor implementation for HermesBackendService
-                    &self.peers.insert("some-transaction-id".to_string(), ctx); //fixme shade
-                });
-            }
-            _ => (),
-        }
+    match result_request_data{
+        Ok(request_string) => {
+            format!("You've sent {:?}", request_string)
+        },
+        Err(_) => String::from("error")
     }
 }
 
-async fn index(
-    req: HttpRequest,
-    stream: web::Payload,
-    data: web::Data<PeerMap<'static>>,
-) -> Result<HttpResponse, Error> {
-    let resp = ws::start(HermesBackendService {}, &req, stream);
-    println!("{:?}", resp);
-    resp
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .app_data(web::Data::new(PeerMap {
-                peers: HashMap::new(),
-            }))
-            .route("/hermes/", web::get().to(index))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![upload])
 }
